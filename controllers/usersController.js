8912b4cd0162a0b41b5d3e1e6ec478816a59df21
Jan <j.c.scherch@gmail.com>
2024-04-09 15:13:18 +0200

    fix: deleteUser controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -68,13 +68,14 @@ const updateUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {
   try {
+    const { id } = req.params;
     const user = await User.findByIdAndDelete(id);
     if (!user) {
       res.status(404).json({ message: "User not found" });
     }
     res
       .status(200)
-      .json({ message: `User "${user.userName}" delteted successfully` });
+      .json({ message: `User ${user.userName} deleted successfully` });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
