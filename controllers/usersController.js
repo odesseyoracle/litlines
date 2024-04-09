3eabc897177f39c42f71fe65f4f972b8e534f3b3
Jan <j.c.scherch@gmail.com>
2024-04-09 13:16:53 +0200

    feature: add getAllUsers function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,24 +1,35 @@
 import User from "../models/usersModel.js";
 
-const register = async (req, res) => {};
-try {
-  const { userName, password, email } = req.body;
+const register = async (req, res) => {
+  try {
+    const { userName, password, email } = req.body;
 
-  const userExists = await User.findOne({ userName });
-  const emailExists = await User.findOne({ email });
-  if (userExists) {
-    return res.status(400).json({ message: "User already exists" });
+    const userExists = await User.findOne({ userName });
+    const emailExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    if (emailExists) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const newUser = { userName, password, email };
+
+    await User.create(newUser);
+
+    res.status(200).json({ message: "New user added 👓", user: userName });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  if (emailExists) {
-    return res.status(400).json({ message: "Email already exists" });
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
+};
 
-  const newUser = { userName, password, email };
-
-  await User.create(newUser);
-
-  res.status(200).json({ message: "New user added👓", user: userName });
-} catch (error) {
-  res.status(500).json({ message: error.message });
-}
 export { register };
