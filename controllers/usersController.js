3104577b8b422f470470849ecd7c73a8263dced6
Jan <j.c.scherch@gmail.com>
2024-04-09 13:29:50 +0200

    feature: add updateUser function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -47,4 +47,23 @@ const getOneUser = async (req, res) => {
   }
 };
 
-export { register, getAllUsers };
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User updated", updatedUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { register, getAllUsers, getOneUser };
