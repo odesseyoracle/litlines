5f45a31dd07f93ef0b1ad237a8eccb82b06bd349
Jan <j.c.scherch@gmail.com>
2024-04-09 11:57:20 +0200

    feature: add user register function

---
+++ controllers/usersController.js
@@ -0,0 +1,24 @@
+import User from "../models/usersModel.js";
+
+const register = async (req, res) => {};
+try {
+  const { userName, password, email } = req.body;
+
+  const userExists = await User.findOne({ userName });
+  const emailExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+  if (emailExists) {
+    return res.status(400).json({ message: "Email already exists" });
+  }
+
+  const newUser = { userName, password, email };
+
+  await User.create(newUser);
+
+  res.status(200).json({ message: "New user added👓", user: userName });
+} catch (error) {
+  res.status(500).json({ message: error.message });
+}
+export { register };
