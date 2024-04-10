4528812b697999c0c52fd3cc48949fb48786bdc2
Jan <j.c.scherch@gmail.com>
2024-04-10 21:17:03 +0200

    feature: add HTML form to LoginPage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <h1>Login</h1>
+      <form>
+        <label htmlFor="userName">Username</label>
+        <input
+          type="text"
+          id="userName"
+          placeholder="Username"
+          name="userName"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          id="password"
+          placeholder="Password"
+          name="password"
+        />
+        <label htmlFor="profilePc">Profile Picture</label>
+        <input type="file" id="profilePic" name="profilePic" />
+        <button>Register</button>
+      </form>
+    </>
+  );
 };
 
 export default LoginPage;
