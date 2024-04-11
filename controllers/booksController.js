ee617a3b7a450e2277d2e3469fd2f39bd0edd238
Jan <j.c.scherch@gmail.com>
2024-04-11 11:07:29 +0200

    feature: add getOneBook controller

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -34,4 +34,17 @@ const getAllBooks = async (req, res) => {
   }
 };
 
-export { addBook };
+const getOneBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    if (!book) {
+      res.status(404).json({ message: "Book not found" });
+    }
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { addBook, getAllBooks, getOneBook };
