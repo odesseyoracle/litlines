1c5f381297f33a3249cb643852f8b42a87f67a74
Jan <j.c.scherch@gmail.com>
2024-04-11 13:55:19 +0200

    feature: add handleSubmit to addBook component

--- client/src/pages/AddBook.jsx
+++ client/src/pages/AddBook.jsx
@@ -3,18 +3,72 @@ import { useState } from "react";
 import axios from "axios";
 
 const AddBook = () => {
+  const [book, setBook] = useState({
+    title: "",
+    author: "",
+    isbn: 0,
+    publisher: "",
+    language: "",
+    bookCover: "",
+  });
+
+  function handleChange(type) {
+    return (e) => {
+      const value = type === "image" ? e.target.files[0] : e.target.value;
+      setBook({ ...book, [e.target.name]: value });
+    };
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault;
+    try {
+      let formData = new FormData();
+      formData.append("title");
+      formData.append("author");
+      formData.append("isbn");
+      formData.append("publisher");
+      formData.append("language");
+      formData.append("bookCover");
+
+      const res = await axios.post(
+        "http://localhost:3000/books/addBook",
+        formData
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <h1>Add a book 📚 </h1>
       <form>
         <label htmlFor="title">Title</label>
-        <input type="text" id="title" name="title" placeholder="Title" />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Title"
+          onChange={handleChange("text")}
+        />
         <br />
         <label htmlFor="author">Author</label>
-        <input type="text" id="author" name="author" placeholder="Author" />
+        <input
+          type="text"
+          id="author"
+          name="author"
+          placeholder="Author"
+          onChange={handleChange("text")}
+        />
         <br />
         <label htmlFor="isbn">ISBN</label>
-        <input type="text" id="isbn" name="isbn" placeholder="ISBN" />
+        <input
+          type="number"
+          id="isbn"
+          name="isbn"
+          placeholder="ISBN"
+          onChange={handleChange("text")}
+        />
         <br />
         <label htmlFor="publisher">Publisher</label>
         <input
@@ -22,6 +76,7 @@ const AddBook = () => {
           id="publisher"
           name="publisher"
           placeholder="Publisher"
+          onChange={handleChange("text")}
         />
         <br />
         <label htmlFor="language">Language</label>
@@ -30,12 +85,18 @@ const AddBook = () => {
           id="language"
           name="language"
           placeholder="Language"
+          onChange={handleChange("text")}
         />
         <br />
         <label htmlFor="bookCover">Book Cover</label>
-        <input type="file" id="bookCover" name="bookCover" />
+        <input
+          type="file"
+          id="bookCover"
+          name="bookCover"
+          onChange={handleChange("image")}
+        />
         <br />
-        <button>Add Book</button>
+        <button onClick={handleSubmit}>Add Book</button>
       </form>
     </>
   );
