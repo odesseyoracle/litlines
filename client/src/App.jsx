65abe0753ecf2d86397fdd5a7070396504830a47
Jan <j.c.scherch@gmail.com>
2024-04-11 15:52:21 +0200

    feature: add navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Navbar from "./features/Navbar.jsx";
 import AddBook from "./pages/AddBook.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   return (
     <>
+      <Navbar />
       <LoginPage />
       <AddBook />
     </>
