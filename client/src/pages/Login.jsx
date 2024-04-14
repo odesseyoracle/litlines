6c21a4867eb78bbc8169e271f2aed406a0279cca
Jan <j.c.scherch@gmail.com>
2024-04-14 13:59:02 +0200

    feature: add submit function to login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export const Login = () => {
-  const handleChange = () => {};
+  const [user, setUser] = useState({
+    userName: "",
+    password: "",
+  });
 
-  const handleSubmit = () => {};
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:3000/users/login", user);
+      console.log("res: ", res);
+      if (res.status == "200") {
+        setUser({
+          userName: "",
+          password: "",
+        });
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log("Error logging in");
+    }
+  };
 
   return (
     <>
