3841f46860b8e23f164d1f1775e8ca223e018d98
Jan <j.c.scherch@gmail.com>
2024-04-16 12:05:30 +0200

    feature: add conditional rendering to showBookInfo

--- client/src/features/Post.jsx
+++ client/src/features/Post.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import UserInfo from "./UserInfo";
 import BookInfo from "../pages/BookInfo.jsx";
 
 const Post = ({ _id, quote, author, page, bookInfo, user }) => {
+  const [showBookInfo, setShowBookInfo] = useState(false);
+
   const handleDelete = async () => {
     try {
       await axios.delete(`http://localhost:4000/posts/${_id}`);
@@ -16,10 +18,15 @@ const Post = ({ _id, quote, author, page, bookInfo, user }) => {
   return (
     <div className="post">
       <UserInfo id={user} />
-      <button onClick={handleDelete}>[X]</button>
+      <button className="exit-button" onClick={handleDelete}>
+        [X]
+      </button>
       <h4>{quote}</h4>
       <p>{author}</p>
-      <BookInfo bookInfo={bookInfo} page={page} />
+      <button onClick={() => setShowBookInfo(!showBookInfo)}>
+        {showBookInfo ? "Hide Book Info" : "Show Book Info"}
+      </button>
+      {showBookInfo && <BookInfo bookInfo={bookInfo} page={page} />}
     </div>
   );
 };
