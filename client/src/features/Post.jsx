f38cdea7993b1a9021044cd690093a0efab23d3f
Jan <j.c.scherch@gmail.com>
2024-04-16 17:26:53 +0200

    fix: edit button in Post

--- client/src/features/Post.jsx
+++ client/src/features/Post.jsx
@@ -3,9 +3,11 @@ import axios from "axios";
 import UserInfo from "./UserInfo";
 import BookInfo from "../pages/BookInfo.jsx";
 import { useAppContext } from "../contexts/AppContext.jsx";
+import EditPostForm from "./EditPostForm.jsx";
 
 const Post = ({ _id, quote, author, page, bookInfo, user }) => {
   const [showBookInfo, setShowBookInfo] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
 
   const { userState, fetchQuotes } = useAppContext();
 
@@ -20,18 +22,24 @@ const Post = ({ _id, quote, author, page, bookInfo, user }) => {
     }
   };
 
+  const handleShowEdit = () => {
+    setShowEdit(!showEdit);
+  };
+
   return (
     <div className="post">
       <UserInfo id={user} />
       {isOwner && (
         <div className="header-button-container">
-          <button className="header-button">[Edit]</button>
+          <button className="header-button" onClick={handleShowEdit}>
+            [Edit]
+          </button>
           <button className="header-button" onClick={handleDelete}>
             [X]
           </button>
         </div>
       )}
-      <h4>{quote}</h4>
+      {showEdit ? <EditPostForm /> : <h4>{quote}</h4>}
       <p>{author}</p>
       <button onClick={() => setShowBookInfo(!showBookInfo)}>
         {showBookInfo ? "Hide Book Info" : "Show Book Info"}
